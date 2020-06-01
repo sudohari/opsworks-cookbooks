12e935f45f3ac43d7d4ef4783baffc899cd648d1
Hari <hariharan.g@lucideustech.com>
2020-06-02 00:20:27 +0530

    credential_changes

--- deploy/definitions/deploy_secrets.rb
+++ deploy/definitions/deploy_secrets.rb
@@ -1,18 +1,20 @@
-Chef::Log.info "wow here"
-Chef::Log.info params.inspect
+define :deploy_secrets do
+  Chef::Log.info "wow here"
+  Chef::Log.info params.inspect
 
-directory "#{params[:path]}/shared/config/credentials" do
-  group params[:group]
-  owner params[:user]
-  mode 0770
-  action :create
-  recursive true
-end
+  directory "#{params[:path]}/shared/config/credentials" do
+    group params[:group]
+    owner params[:user]
+    mode 0770
+    action :create
+    recursive true
+  end
 
-template "#{params[:deploy_to]}/shared/config/credentials/#{params[:environment]}.key" do
-  cookbook "rails"
-  source "environment.key.erb"
-  mode "0660"
-  owner params[:user]
-  group params[:group]
+  template "#{params[:deploy_to]}/shared/config/credentials/#{params[:environment]}.key" do
+    cookbook "rails"
+    source "environment.key.erb"
+    mode "0660"
+    owner params[:user]
+    group params[:group]
+  end
 end
