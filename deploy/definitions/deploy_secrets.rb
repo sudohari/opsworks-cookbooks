03082d424d858d3c0cde1032e0249bd9d502ba6c
Hari <hariharan.g@lucideustech.com>
2020-06-02 00:51:11 +0530

    dir_already_exists

--- deploy/definitions/deploy_secrets.rb
+++ deploy/definitions/deploy_secrets.rb
@@ -1,15 +1,15 @@
 define :deploy_secrets do
-  Chef::Log.info "wow here"
+  Chef::Log.info "Params & Node"
   Chef::Log.info params.inspect
   Chef::Log.info node.inspect
 
-  directory "#{params[:path]}/shared/config/credentials" do
-    group params[:group]
-    owner params[:user]
-    mode 0770
-    action :create
-    recursive true
-  end
+  # directory "#{params[:path]}/shared/config/credentials" do
+  #   group params[:group]
+  #   owner params[:user]
+  #   mode 0770
+  #   action :create
+  #   recursive true
+  # end
 
   template "#{params[:path]}/shared/config/credentials/#{params[:environment]}.key" do
     cookbook "deploy"
